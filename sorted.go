f94d49710556f2eef0cb460208206fb3886e0c2b
forjoin92 <forjoin92@gmail.com>
2018-12-21 12:12:30 +0800

    add selection sort

--- sorted.go
+++ sorted.go
@@ -1,13 +1,38 @@
 package sorted
 
+// BubbleSort bubble sort
 func BubbleSort(array []int) {
 	l := len(array)
 
-	for i := 0; i < l - 1; i++ {
-		for j := 0; j < l - 1 - i; j++ {
+	if l <= 1 {
+		return
+	}
+
+	for i := 0; i < l-1; i++ {
+		for j := 0; j < l-1-i; j++ {
 			if array[j] > array[j+1] {
 				array[j], array[j+1] = array[j+1], array[j]
 			}
 		}
+	}
+}
+
+// SelectionSort selection sort
+func SelectionSort(array []int) {
+	l := len(array)
+
+	if l <= 1 {
+		return
+	}
+
+	for i := 0; i < l; i++ {
+		temp := i
+
+		for j := i; j < l; j++ {
+			if array[j] < array[temp] {
+				temp = j
+			}
+		}
+		array[i], array[temp] = array[temp], array[i]
 	}
 }
--- sorted_test.go
+++ sorted_test.go
@@ -1,10 +1,21 @@
 package sorted
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestBubbleSort(t *testing.T) {
 	var array = []int{8, 10, 4, 3, 1, 9, 6, 2, 7, 5}
 
 	BubbleSort(array)
-	t.Log(array)
+	assert.Equal(t, array, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+}
+
+func TestSelectionSort(t *testing.T) {
+	var array = []int{8, 10, 4, 3, 1, 9, 6, 2, 7, 5}
+
+	SelectionSort(array)
+	assert.Equal(t, array, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
 }
